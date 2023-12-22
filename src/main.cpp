5f17961512961331d5e5652e97069290fb530f3e
Space Pirate <b22ee065@iitj.ac.in>
2023-12-22 15:14:43 +0530

    fps remains

--- src/main.cpp
+++ src/main.cpp
@@ -1,98 +1,89 @@
 #include <iostream>
 #include <SDL.h>
-#include <SDL_ttf.h>
-#include <string>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 const int CIRCLE_RADIUS = 20;
+float dampening = 0.9f; // Dampening factor
+float gravity = 0.1f; // Gravity factor
 
 class Circle {
 public:
-    Circle(float x, float y, float radius, float vx, float vy) : x(x), y(y), radius(radius), vx(vx), vy(vy) {
-        font = TTF_OpenFont("arial.ttf", 16); // Try to open the specified font
-        if (!font) {
-            std::cerr << "Error: Unable to load font - " << TTF_GetError() << std::endl;
-            std::cerr << "Using default font." << std::endl;
-
-            // Use a default font (e.g., system font)
-            font = TTF_OpenFont(nullptr, 16);
-            if (!font) {
-                std::cerr << "Error: Unable to load default font - " << TTF_GetError() << std::endl;
-                // Handle the error, exit, or use a different default font
-            }
-        }
-    }
-
-    ~Circle() {
-        TTF_CloseFont(font);
-    }
+    Circle(float x, float y, float radius, float vx, float vy) : x(x), y(y), radius(radius), vx(vx), vy(vy) {}
 
     void update() {
         x += vx;
+        vy += gravity;
         y += vy;
 
-        float dampening = 0.9f;
-
-        if (x - radius < 0) {
+        // Check for collisions with the bounding box
+        if (x - radius < 0){
             x = radius;
-            vx = -vx * dampening;
-        } else if (x + radius > SCREEN_WIDTH) {
-            x = SCREEN_WIDTH - radius;
-            vx = -vx * dampening;
+            vx = -vx*dampening; // Reflect velocity on X-axis
         }
-        if (y - radius < 0) {
+        else if (x + radius > SCREEN_WIDTH) {
+            x = SCREEN_WIDTH - radius;
+            vx = -vx*dampening; // Reflect velocity on X-axis
+        }
+        if(y - radius < 0){
             y = radius;
-            vy = -vy * dampening;
-        } else if (y + radius > SCREEN_HEIGHT) {
+            vy = -vy*dampening; // Reflect velocity on Y-axis
+        }
+        else if (y + radius > SCREEN_HEIGHT) {
             y = SCREEN_HEIGHT - radius;
-            vy = -vy * dampening;
+            vy = -vy*dampening; // Reflect velocity on Y-axis
         }
     }
 
     void render(SDL_Renderer* renderer) const {
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White color
         draw_circle(renderer, x, y, radius);
-
-        SDL_Rect textRect;
-        textRect.x = SCREEN_WIDTH - 100;
-        textRect.y = 10;
-        textRect.w = 90;
-        textRect.h = 20;
-
-        std::string velocityText = "Vx: " + std::to_string(vx) + " Vy: " + std::to_string(vy);
-        render_text(renderer, velocityText.c_str(), textRect);
     }
 
 private:
-    float x, y;
+    float x, y; // Circle center coordinates
     float radius;
-    float vx, vy;
-    TTF_Font* font;
+    float vx, vy; // Velocity components
 
+    // Function to draw a circle on the renderer
     void draw_circle(SDL_Renderer* renderer, float x, float y, float radius) const {
-        // (Unchanged) Function to draw a circle on the renderer
-    }
+        float d = 3 - 2 * radius;
+        float xPos = 0;
+        float yPos = radius;
 
-    void render_text(SDL_Renderer* renderer, const char* text, SDL_Rect rect) const {
-        SDL_Surface* surface = nullptr;
-        SDL_Texture* texture = nullptr;
+        while (yPos >= xPos) {
+            SDL_RenderDrawPoint(renderer, x + xPos, y - yPos);
+            SDL_RenderDrawPoint(renderer, x - xPos, y - yPos);
+            SDL_RenderDrawPoint(renderer, x + xPos, y + yPos);
+            SDL_RenderDrawPoint(renderer, x - xPos, y + yPos);
+            SDL_RenderDrawPoint(renderer, x + yPos, y - xPos);
+            SDL_RenderDrawPoint(renderer, x - yPos, y - xPos);
+            SDL_RenderDrawPoint(renderer, x + yPos, y + xPos);
+            SDL_RenderDrawPoint(renderer, x - yPos, y + xPos);
 
-        SDL_Color textColor = {255, 255, 255, 255};
+            xPos++;
 
-        surface = TTF_RenderText_Solid(font, text, textColor);
-        texture = SDL_CreateTextureFromSurface(renderer, surface);
-
-        SDL_RenderCopy(renderer, texture, NULL, &rect);
-
-        SDL_FreeSurface(surface);
-        SDL_DestroyTexture(texture);
+            if (d > 0) {
+                yPos--;
+                d = d + 4 * (xPos - yPos) + 10;
+            } else {
+                d = d + 4 * xPos + 6;
+            }
+        }
     }
 };
 
 int main() {
+    std::cout << "Enter dampening factor (e): ";
+    std::cin >> dampening;
+    std::cout << "Enter gravity factor: ";
+    std::cin >> gravity;
+    // check dampening factor is between 0 and 1
+    if (dampening < 0 || dampening > 1) {
+        std::cerr << "Dampening factor must be between 0 and 1" << std::endl;
+        return 1;
+    }
     SDL_Init(SDL_INIT_VIDEO);
-    TTF_Init();
 
     SDL_Window* window = SDL_CreateWindow("2D Physics Engine", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
@@ -111,19 +102,18 @@ int main() {
 
         circle.update();
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // Set the clear color to black
+        SDL_RenderClear(renderer); // Clear the renderer
 
         circle.render(renderer);
 
         SDL_RenderPresent(renderer);
-        SDL_Delay(16);
+        SDL_Delay(16); // Add a small delay to control the frame rate
     }
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
-    TTF_Quit();
 
     return 0;
 }
