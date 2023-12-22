2b72349e75c1d702625d17fb1b2f9c4b6de0bff9
Space Pirate <b22ee065@iitj.ac.in>
2023-12-22 15:40:25 +0530

    Added mouse functionality

--- src/main.cpp
+++ src/main.cpp
@@ -10,33 +10,49 @@ float gravity = 0.1f;   // Gravity factor
 class Circle {
 public:
     Circle(float x, float y, float radius, float vx, float vy)
-        : x(x), y(y), radius(radius), vx(vx), vy(vy) {}
+        : x(x), y(y), radius(radius), vx(vx), vy(vy), selected(false) {}
 
     void update() {
-        x += vx;
-        vy += gravity;
-        y += vy;
+        if (selected) {
+            // If the circle is selected, update its position based on the mouse
+            int mouseX, mouseY;
+            SDL_GetMouseState(&mouseX, &mouseY);
+            x = static_cast<float>(mouseX);
+            y = static_cast<float>(mouseY);
+        } else {
+            x += vx;
+            vy += gravity;
+            y += vy;
 
-        // Check for collisions with the bounding box
-        if (x - radius < 0) {
-            x = radius;
-            vx = -vx * dampening; // Reflect velocity on X-axis
-        } else if (x + radius > SCREEN_WIDTH) {
-            x = SCREEN_WIDTH - radius;
-            vx = -vx * dampening; // Reflect velocity on X-axis
-        }
-        if (y - radius < 0) {
-            y = radius;
-            vy = -vy * dampening; // Reflect velocity on Y-axis
-        } else if (y + radius > SCREEN_HEIGHT) {
-            y = SCREEN_HEIGHT - radius;
-            vy = -vy * dampening; // Reflect velocity on Y-axis
+            // Check for collisions with the bounding box
+            if (x - radius < 0) {
+                x = radius;
+                vx = -vx * dampening; // Reflect velocity on X-axis
+            } else if (x + radius > SCREEN_WIDTH) {
+                x = SCREEN_WIDTH - radius;
+                vx = -vx * dampening; // Reflect velocity on X-axis
+            }
+            if (y - radius < 0) {
+                y = radius;
+                vy = -vy * dampening; // Reflect velocity on Y-axis
+            } else if (y + radius > SCREEN_HEIGHT) {
+                y = SCREEN_HEIGHT - radius;
+                vy = -vy * dampening; // Reflect velocity on Y-axis
+            }
         }
     }
 
     void render(SDL_Renderer* renderer) const {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White color
         draw_circle(renderer, x, y, radius);
+    }
+
+    void select() {
+        selected = true;
+    }
+
+    void unselect() {
+        selected = false;
     }
 
     // Function to draw a circle on the renderer
@@ -75,24 +91,34 @@ public:
         return y;
     }
 
-    // Function to set the position of the circle
-    void setPosition(float newX, float newY) {
-        x = newX;
-        y = newY;
+    // Function to check if the circle is selected
+    bool isSelected() const {
+        return selected;
     }
 
 private:
     float x, y;            // Circle center coordinates
     float radius;
     float vx, vy;          // Velocity components
+    bool selected;         // Flag to indicate if the circle is selected
 };
 
 int main() {
-    std::cout << "Enter dampening factor (e): ";
-    std::cin >> dampening;
-    std::cout << "Enter gravity factor: ";
-    std::cin >> gravity;
-    // check dampening factor is between 0 and 1
+    std::cout << "Enter dampening factor (e, default 0.9): ";
+    std::string dampeningInput;
+    std::getline(std::cin, dampeningInput);
+
+    // Use default value if the input is empty
+    dampening = dampeningInput.empty() ? 0.9f : std::stof(dampeningInput);
+
+    std::cout << "Enter gravity factor (default 0.1): ";
+    std::string gravityInput;
+    std::getline(std::cin, gravityInput);
+
+    // Use default value if the input is empty
+    gravity = gravityInput.empty() ? 0.1f : std::stof(gravityInput);
+
+    // Check dampening factor is between 0 and 1
     if (dampening < 0 || dampening > 1) {
         std::cerr << "Dampening factor must be between 0 and 1" << std::endl;
         return 1;
@@ -109,7 +135,6 @@ int main() {
     Circle circle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, CIRCLE_RADIUS, 5, 5);
 
     bool quit = false;
-    bool paused = false;
     SDL_Event event;
 
     while (!quit) {
@@ -118,31 +143,30 @@ int main() {
                 quit = true;
             } else if (event.type == SDL_KEYDOWN) {
                 if (event.key.keysym.sym == SDLK_SPACE) {
-                    paused = !paused; // Toggle pause state
-                } else if (paused) {
-                    // If paused, allow the user to change the position
-                    // of the circle using arrow keys
-                    switch (event.key.keysym.sym) {
-                        case SDLK_UP:
-                            circle.setPosition(circle.getX(), circle.getY() - 5);
-                            break;
-                        case SDLK_DOWN:
-                            circle.setPosition(circle.getX(), circle.getY() + 5);
-                            break;
-                        case SDLK_LEFT:
-                            circle.setPosition(circle.getX() - 5, circle.getY());
-                            break;
-                        case SDLK_RIGHT:
-                            circle.setPosition(circle.getX() + 5, circle.getY());
-                            break;
+                    if (!circle.isSelected()) {
+                        circle.select(); // Select the circle on spacebar press
+                    } else {
+                        circle.unselect(); // Unselect the circle on spacebar press
+                    }
+                }
+            } else if (event.type == SDL_MOUSEBUTTONDOWN && event.button.button == SDL_BUTTON_LEFT) {
+                // Left-click to toggle the selection of the circle
+                int mouseX, mouseY;
+                SDL_GetMouseState(&mouseX, &mouseY);
+                float circleX = circle.getX();
+                float circleY = circle.getY();
+                if (mouseX >= circleX - CIRCLE_RADIUS && mouseX <= circleX + CIRCLE_RADIUS &&
+                    mouseY >= circleY - CIRCLE_RADIUS && mouseY <= circleY + CIRCLE_RADIUS) {
+                    if (!circle.isSelected()) {
+                        circle.select();
+                    } else {
+                        circle.unselect();
                     }
                 }
             }
         }
 
-        if (!paused) {
-            circle.update();
-        }
+        circle.update();
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
