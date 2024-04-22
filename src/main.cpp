8c1afa519c865d8e9a01e4d830616cf7d67cc6c1
Rakshitx1 <rakshit1@gmail.com>
2024-04-23 03:25:40 +0530

    added jump and removed image lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
 
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
@@ -8,45 +7,56 @@ const int CIRCLE_RADIUS = 20;
 float dampening = 0.9f; // Dampening factor
 float gravity = 0.1f;   // Gravity factor
 
-class Circle {
+class Circle
+{
 public:
     Circle(float x, float y, float radius, float vx, float vy)
         : x(x), y(y), radius(radius), vx(vx), vy(vy) {}
 
-    void update() {
+    void update()
+    {
         x += vx;
         vy += gravity;
         y += vy;
 
         // Check for collisions with the bounding box
-        if (x - radius < 0) {
+        if (x - radius < 0)
+        {
             x = radius;
             vx = -vx * dampening; // Reflect velocity on X-axis
-        } else if (x + radius > SCREEN_WIDTH) {
+        }
+        else if (x + radius > SCREEN_WIDTH)
+        {
             x = SCREEN_WIDTH - radius;
             vx = -vx * dampening; // Reflect velocity on X-axis
         }
-        if (y - radius < 0) {
+        if (y - radius < 0)
+        {
             y = radius;
             vy = -vy * dampening; // Reflect velocity on Y-axis
-        } else if (y + radius > SCREEN_HEIGHT) {
+        }
+        else if (y + radius > SCREEN_HEIGHT)
+        {
             y = SCREEN_HEIGHT - radius;
             vy = -vy * dampening; // Reflect velocity on Y-axis
         }
     }
 
-    void render(SDL_Renderer* renderer) const {
+    void render(SDL_Renderer *renderer) const
+    {
         SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // White color
         draw_circle(renderer, x, y, radius);
     }
 
     // Function to draw a circle on the renderer
-    void draw_circle(SDL_Renderer* renderer, float x, float y, float radius) const {
+    void draw_circle(SDL_Renderer *renderer, float x, float y, float radius) const
+    {
         float d = 3 - 2 * radius;
         float xPos = 0;
         float yPos = radius;
 
-        while (yPos >= xPos) {
+        while (yPos >= xPos)
+        {
             SDL_RenderDrawPoint(renderer, x + xPos, y - yPos);
             SDL_RenderDrawPoint(renderer, x - xPos, y - yPos);
             SDL_RenderDrawPoint(renderer, x + xPos, y + yPos);
@@ -58,42 +68,50 @@ public:
 
             xPos++;
 
-            if (d > 0) {
+            if (d > 0)
+            {
                 yPos--;
                 d = d + 4 * (xPos - yPos) + 10;
-            } else {
+            }
+            else
+            {
                 d = d + 4 * xPos + 6;
             }
         }
     }
 
+    void jump()
+    {
+        vy = -5;
+    }
+
     // Getter functions for x and y positions
-    float getX() const {
+    float getX() const
+    {
         return x;
     }
 
-    float getY() const {
+    float getY() const
+    {
         return y;
     }
 
     // Function to set the position of the circle
-    void setPosition(float newX, float newY) {
+    void setPosition(float newX, float newY)
+    {
         x = newX;
         y = newY;
     }
 
 private:
-    float x, y;            // Circle center coordinates
+    float x, y; // Circle center coordinates
     float radius;
-    float vx, vy;          // Velocity components
+    float vx, vy; // Velocity components
 };
 
-int main(int argc, char* argv[]) {
-    // check SDL Image version
-    SDL_version img_version;
-    SDL_IMAGE_VERSION(&img_version);
-    std::cout << "SDL Image version: " << (int)img_version.major << "." << (int)img_version.minor << "." << (int)img_version.patch << std::endl;
-    
+int main(int argc, char *argv[])
+{
+
     // check SDL version
     SDL_version sdl_version;
     SDL_VERSION(&sdl_version);
@@ -114,17 +132,18 @@ int main(int argc, char* argv[]) {
     gravity = gravityInput.empty() ? 0.1f : std::stof(gravityInput);
 
     // Check dampening factor is between 0 and 1
-    if (dampening < 0 || dampening > 1) {
+    if (dampening < 0 || dampening > 1)
+    {
         std::cerr << "Dampening factor must be between 0 and 1" << std::endl;
         return 1;
     }
     SDL_Init(SDL_INIT_VIDEO);
 
-    SDL_Window* window =
+    SDL_Window *window =
         SDL_CreateWindow("2D Physics Engine", SDL_WINDOWPOS_CENTERED,
                          SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT,
                          SDL_WINDOW_SHOWN);
-    SDL_Renderer* renderer =
+    SDL_Renderer *renderer =
         SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
     Circle circle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, CIRCLE_RADIUS, 5, 5);
@@ -133,35 +152,53 @@ int main(int argc, char* argv[]) {
     bool paused = false;
     SDL_Event event;
 
-    while (!quit) {
-        while (SDL_PollEvent(&event) != 0) {
-            if (event.type == SDL_QUIT) {
+    while (!quit)
+    {
+        while (SDL_PollEvent(&event) != 0)
+        {
+            if (event.type == SDL_QUIT)
+            {
                 quit = true;
-            } else if (event.type == SDL_KEYDOWN) {
-                if (event.key.keysym.sym == SDLK_SPACE) {
+            }
+            else if (event.type == SDL_KEYDOWN)
+            {
+                if (event.key.keysym.sym == SDLK_RSHIFT)
+                {
+                    circle.jump(); // Jump when right shift is pressed
+                }
+                if (event.key.keysym.sym == SDLK_SPACE)
+                {
                     paused = !paused; // Toggle pause state
-                } else if (paused) {
+                }
+                else if (paused)
+                {
                     // If paused, allow the user to change the position
                     // of the circle using arrow keys
-                    switch (event.key.keysym.sym) {
-                        case SDLK_UP:
-                            circle.setPosition(circle.getX(), circle.getY() - 5);
-                            break;
-                        case SDLK_DOWN:
-                            circle.setPosition(circle.getX(), circle.getY() + 5);
-                            break;
-                        case SDLK_LEFT:
-                            circle.setPosition(circle.getX() - 5, circle.getY());
-                            break;
-                        case SDLK_RIGHT:
-                            circle.setPosition(circle.getX() + 5, circle.getY());
-                            break;
+                    switch (event.key.keysym.sym)
+                    {
+                    case SDLK_UP:
+                        circle.setPosition(circle.getX(), circle.getY() - 5);
+                        break;
+                    case SDLK_DOWN:
+                        circle.setPosition(circle.getX(), circle.getY() + 5);
+                        break;
+                    case SDLK_LEFT:
+                        circle.setPosition(circle.getX() - 5, circle.getY());
+                        break;
+                    case SDLK_RIGHT:
+                        circle.setPosition(circle.getX() + 5, circle.getY());
+                        break;
+                    case SDLK_r: // Reset the position of the circle
+                        circle.setPosition(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+                        break;
+                        // case SDLK_
                     }
                 }
             }
         }
 
-        if (!paused) {
+        if (!paused)
+        {
             circle.update();
         }
 
